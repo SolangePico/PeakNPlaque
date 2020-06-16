95915442f6b04741fb9bf1305806dbd0fac8cffb
Solange <solangebeatlespauljohn@hotmail.com>
2020-06-16 17:23:05 -0500

    Creation of the program structure (classes).

---
+++ src/peaknplaque/PeakNPlaque.java
@@ -0,0 +1,24 @@
+/**
+ * This program was created with the purpose of seeing the skills that the
+ * programmer in question has, non-profit, for Stackbuilders Company.
+ *
+ * PeakNPlaque Predictor / Pico y Placa 
+ * Predictor 
+ * stackbuilders 2020 (c)
+ */
+package peaknplaque;
+
+/**
+ *
+ * @author Solange Pico
+ */
+public class PeakNPlaque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to the 'Pico y Placa' predictive system. ");
+    }
+}
